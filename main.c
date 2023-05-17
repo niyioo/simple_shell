85c5900518b5f13131f17d1863b095837b321d3e
Abraham0625 <adesanya547@gmail.com>
2023-05-17 04:57:58 -0700

    second push

--- main.c
+++ main.c
@@ -1,29 +1,76 @@
 #include "main.h"
 
+void execmd(char **argv);
+
 int main(int ac, char **argv){
-  char *prompt = "#cisfun$ ";
-  char *lineptr;
-  size_t n = 0; 
-  ssize_t nchars_read;
+	char *prompt = "#cisfun$ ";
+	char *lineptr = NULL, *lineptr_copy = NULL;
+	size_t n = 0;
+	ssize_t nchars_read;
+	const char *delim = " \n";
+	int num_tokens = 0;
+	char *token;
+	int i;
+	
+	/* declaring void variables */
+	(void)ac;
+	
+	/* Create a loop for the shell's prompt */
+	while (1){
+		printf("%s", prompt);
+		nchars_read = getline(&lineptr, &n, stdin);
+		/* check if the getline function failed or reached EOF or user use CTRL + D */
+		if (nchars_read == -1){
+			printf("Exiting shell....\n");
+			return (-1);
+		}
+		
+		/* allocate space for a copy of the lineptr */
+		
+		lineptr_copy = malloc(sizeof(char) * nchars_read);
+		if (lineptr_copy== NULL){
+			perror("tsh: memory allocation error");
+			return (-1);
+		}
+		/* copy lineptr to lineptr_copy */
+		
+		strcpy(lineptr_copy, lineptr);
+		
+		/********** split the string (lineptr) into an array of words ********/
+		/* calculate the total number of tokens */
+		token = strtok(lineptr, delim);
+		
+		while (token != NULL){
+			num_tokens++;
+			token = strtok(NULL, delim);
+		}
+		num_tokens++;
+		
+		/* Allocate space to hold the array of strings */
+		argv = malloc(sizeof(char *) * num_tokens);
+		
+		/* Store each token in the argv array */
+		token = strtok(lineptr_copy, delim);
+		
+		for (i = 0; token != NULL; i++){
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+			
+			token = strtok(NULL, delim);
 
-   /* declaring void variables */
-  (void)ac; (void)argv;
+		}
+		argv[i] = NULL;
 
-    /* create an infinite loop */
-    while (1){
-      printf("%s", prompt);
-      nchars_read = getline(&lineptr, &n, stdin);
-      /* check if the getline function failed or reached EOF or user use CTRL + D */ 
-        if (nchars_read == -1){
-            printf("Exiting shell....\n");
-            return (-1);
-        }
+		/* execute the command */
 
-      printf("%s\n", lineptr);
+		execmd(argv);
+		
+	}
+		
+		/* free up allocated memory */
+		free(lineptr_copy);
+		free(lineptr);
+		
+		return (0);
 
-      /* free up allocated memory */
-      free(lineptr);    
-    }
-
-  return (0);
 }
