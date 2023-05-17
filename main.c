0357f22e2ad976a1e34c0bc6344f07a29c5a62e3
Abraham0625 <adesanya547@gmail.com>
2023-05-17 02:25:28 -0700

    first push

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int main(int ac, char **argv){
+  char *prompt = "#cisfun$ ";
+  char *lineptr;
+  size_t n = 0; 
+  ssize_t nchars_read;
+
+   /* declaring void variables */
+  (void)ac; (void)argv;
+
+    /* create an infinite loop */
+    while (1){
+      printf("%s", prompt);
+      nchars_read = getline(&lineptr, &n, stdin);
+      /* check if the getline function failed or reached EOF or user use CTRL + D */ 
+        if (nchars_read == -1){
+            printf("Exiting shell....\n");
+            return (-1);
+        }
+
+      printf("%s\n", lineptr);
+
+      /* free up allocated memory */
+      free(lineptr);    
+    }
+
+  return (0);
+}
